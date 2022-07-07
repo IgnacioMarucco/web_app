fe3c2ab593e41b7b3c5f6851b94f4374274b8078
ignacio <ignaciomarucco@gmail.com>
2022-07-07 17:08:27 -0300

    Add login

---
+++ js/login.js
@@ -0,0 +1,81 @@
+const formularioReg = document.getElementById(`formularioReg`);
+formularioReg.addEventListener(`submit`, storeData);
+const formularioLogin = document.getElementById(`formularioLogin`);
+formularioLogin.addEventListener(`submit`, tryLogin);
+
+const arrayDatos = [];
+
+// Funcion storeData
+function storeData(e) {
+    e.preventDefault();
+    const nombreReg = document.getElementById(`nombreReg`).value;
+    const telefonoReg = document.getElementById(`telefonoReg`).value;
+    const emailReg = document.getElementById(`emailReg`).value;
+    const passwordReg = document.getElementById(`passwordReg`).value;
+
+    const datosRegistro = {
+        nombreReg,
+        telefonoReg,
+        emailReg,
+        passwordReg
+    }
+
+    const datosRegistroJSON = JSON.stringify(datosRegistro);
+    
+    localStorage.setItem(`datosRegistro`, datosRegistroJSON);
+    arrayDatos.push(datosRegistro);
+    const arrayDatosJSON = JSON.stringify(arrayDatos);
+    localStorage.setItem(`arrayDatos`, arrayDatosJSON);
+}
+
+// Funcion tryLogin
+function tryLogin(e) {
+    e.preventDefault(); 
+
+    const emailLogin = document.getElementById(`emailLogin`).value;
+    const passwordLogin = document.getElementById(`passwordLogin`).value;
+
+    const datosRegistro = JSON.parse(localStorage.getItem(`arrayDatos`));
+    console.log(datosRegistro);
+
+    let usuario = datosRegistro.find((usuarioRegistrado) => usuarioRegistrado.emailReg == emailLogin);
+    console.log(usuario)
+    if(!usuario){
+        console.log(`Datos ingresados incorrectos.`);
+    } else if (usuario.passwordReg == passwordLogin){
+        const nombreLogueado = document.getElementById(`nombreLogueado`);
+        nombreLogueado.innerHTML = `${usuario.nombreReg}`;
+        console.log(nombreLogueado.innerHTML)
+        // console.log(`Bienvenido de vuelta, ${usuario.nombreReg}.`);
+    }else {
+        console.log(`Datos ingresados incorrectos.`);
+    }
+}
+
+// const nombreLogueado = document.getElementById(`nombreLogueado`);
+// nombreLogueado.innerHTML = ``;
+
+
+// Validaciones test:
+// const emailLogin = document.getElementById(`emailLogin`);
+// const passwordLogin = document.getElementById(`passwordLogin`);
+
+
+// emailLogin.addEventListener(`blur`, () => {
+//     const error = document.getElementById(`errorEmailLogin`);
+//     if(emailLogin.value.length > 0 && emailLogin.value.includes(`@`) && emailLogin.value.includes(`.`)){
+//         error.innerHTML = ``;
+//     } else {
+//         error.innerHTML = `Formato de Email incorrecto`;
+//     }
+// })
+
+
+// passwordLogin.addEventListener(`blur`, () => {
+//     const error = document.getElementById(`errorPasswordLogin`);
+//     if(passwordLogin.value.length > 0){
+//         error.innerHTML = ``;
+//     } else {
+//         error.innerHTML = `Formato de contraseña incorrecto.`;
+//     }
+// });
