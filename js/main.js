789cec4498527a5575ce6935ddb4cad24f966b43
ignacio <ignaciomarucco@gmail.com>
2022-07-09 16:00:55 -0300

    Buscador con evento click

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
 //Segunda entrega de proyecto final: JSON + Storage + DOM + Eventos
-
 import {arrayProductos} from './productos.js';
 import { agregarCarro } from './carro.js';
 import { mostrarCarro } from './carro.js';
-
 import {arrayDatos, registro, login} from './usuario.js';
 
-// Mostrar grilla de productos en el HTML:
+
+// // Funcion para obtener los formatos de cada producto
+// const mostrarFormatos2 = (id) => {
+// 	const formatoTexto = document.createElement(`div`);
+// 	formatoTexto.innerHTML = `Formatos: <br>`;
+// 	const producto = arrayProductos.find((producto) => producto.id == id);
+// 	console.log(`producto ${id}`,producto);
+
+// 	// console.log(producto);
+// 	const productoFormatos = producto.formatos;
+// 	// console.log(productoFormatos);
+// 	productoFormatos.forEach((formato) => {
+// 		// console.log(formato[0]);
+// 		// console.log(formato[1]);
+// 		formatoTexto.innerHTML += `<span><label for="${formato[1]}">${formato[0]} gr Precio: $${formato[1]}</label><input type="radio" name="precioProducto${producto.id}" value="${formato[1]}"><span>`;
+// 	});
+// 	// console.log(formatoTexto)
+// 	return formatoTexto;
+// }
+
+// Mostrar grid de productos en el HTML manipulando el DOM:
 const mostrarProductos = () => {
 	let gridProductos = document.getElementById(`grid-productos`);
 	gridProductos.innerHTML = ``;
-	// Tomo como padre ese div, ahora genero a partir del arrayProductos una grid completa de productos.
 
 	// Le agrego un filtro de productos
-	filtrarProductos();
 	let arrayProductosFiltrados = filtrarProductos();
+
+	// Ahora creo las cards de cada producto a partir del array de productos filtrados.
 	for (const producto of arrayProductosFiltrados) {
 		let containerCard = document.createElement(`div`);
-
 		containerCard.className = `col`;
+
+		// let formatos = mostrarFormatos2(producto.id);
+		// console.log(`prueba`, formatos)
+
 		containerCard.innerHTML = `<div class="card align-items-center">
 									<h4 class="card-title">${producto.nombre}</h4>
 									<img class="card-img-top" src="../public/img_prod/${producto.id}.webp" alt="Imagen de ${producto.nombre}">
 									<h6>$${producto.precio}</h6>
+									<!--- {formatos.innerHTML} -->
 									<a id="${producto.id}" class="btn btn-primary btnAgregarCarro">Agregar al carro</a>
 									<div>`;
 		gridProductos.appendChild(containerCard);
@@ -30,7 +52,6 @@ const mostrarProductos = () => {
 
 	if (arrayProductosFiltrados.length === 0) {
 		gridProductos.innerHTML = `<h2>No hay coincidencias!</h2>`;
-		console.log(`Sin coincidencias`);
 	}
 
 	// Agrego el event listener a cada boton (que tienen id unico), para pushear el producto comprado al arrayCarro
@@ -41,12 +62,27 @@ const mostrarProductos = () => {
 
 // Funcion para filtrar los productos a mostrar:
 const filtrarProductos = () => {
+	// Defino el campo de busqueda y le agrego el evento para mostrar los productos buscados.
+	const buscador = document.getElementById(`buscador`);
+	const buscadorBtn = document.getElementById(`buscadorBtn`);
+	buscadorBtn.addEventListener(`click`, mostrarProductos);
 	let arrayProductosFiltrados = arrayProductos.filter(elemento => elemento.nombre.toLowerCase().includes(`${buscador.value.toLowerCase()}`));
 	return arrayProductosFiltrados;
 }
-// Defino el campo de busqueda y le agrego el evento para mostrar los productos buscados.
-const buscador = document.getElementById(`buscador`);
-buscador.addEventListener(`input`, mostrarProductos);
-
 
 window.onload = mostrarProductos();
+
+
+
+
+
+// Funcion para mostrar los formatos disponibles de cada producto en cada card:
+// const mostrarFormatos = () => {
+// 	// arrayProductos.forEach((producto)=> console.log(producto));
+// 	arrayProductos.forEach((producto) => {
+// 		producto.formatos.forEach((formato)=> {
+// 			console.log(formato);
+// 		})
+// 	})
+// }
+// mostrarFormatos();
--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,21 @@
 // Clase constructora para productos:
 class Producto {
-	constructor(id, nombre, precio, cantidad) {
+	constructor(id, nombre, precio, cantidad, formatos) {
 		this.id = id;
 		this.nombre = nombre;
 		this.precio = precio;
 		this.cantidad = cantidad;
+    this.formatos = formatos;
 	}
 }
 
 // Productos disponibles:
-const eucalipto = new Producto(1, `Miel de Eucalipto`, 100, 0);
-const algarrobo = new Producto(2, `Miel de Algarrobo`, 150, 0);
-const citrus = new Producto(3, `Miel de Citrus`, 200, 0);
-const girasol = new Producto(4, `Miel de Girasol`, 250, 0);
-const polen = new Producto(5, `Polen de Abeja`, 200, 0);
-const propoleo = new Producto(6,`Propoleo`, 300, 0);
-
+const eucalipto = new Producto(1, `Miel de Eucalipto`, 100, 0, [[1000,800],[500,450]]);
+const algarrobo = new Producto(2, `Miel de Algarrobo`, 150, 0, [[1000,850],[500,450]]);
+const citrus = new Producto(3, `Miel de Citrus`, 200, 0, [[1000,750],[500,450]]);
+const girasol = new Producto(4, `Miel de Girasol`, 250, 0,[[1000,800],[500,450]]);
+const polen = new Producto(5, `Polen de Abeja`, 200, 0,[[500,500]]);
+const propoleo = new Producto(6,`Propoleo`, 300, 0,[[250,500]]);
 
 
 // Agregar productos al Array de Productos DISPONIBLES
