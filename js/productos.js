3b4978d25dd14ed68665566787ef75e7c25fb1e4
ignacio <ignaciomarucco@gmail.com>
2022-07-23 17:25:39 -0300

    rutas

--- js/productos.js
+++ js/productos.js
@@ -1,7 +1,7 @@
 import { mostrarProductos } from "../js/main.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch("data.json")
+  fetch("../js/data.json")
     .then((response) => response.json())
     .then((data) => {
       mostrarProductos(data);
