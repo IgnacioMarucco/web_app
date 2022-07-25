a60c8006bacdbc5104e8adcb5482ee5c72040233
ignacio <ignaciomarucco@gmail.com>
2022-07-25 16:24:10 -0300

    Add event DOMContentLoaded

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-import { mostrarProductos } from "../js/main.js";
+import { mostrarProductos } from "./main.js";
 import { existeCarro } from "../js/carro.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch("../js/data_productos.json")
+  fetch("js/data_productos.json")
     .then((response) => response.json())
     .then((data_productos) => {
       mostrarProductos(data_productos);
       existeCarro();
   })
 })
+
