55898dd004f243d66e60da97e8585f4fd5c40629
ignacio <ignaciomarucco@gmail.com>
2022-07-25 16:57:03 -0300

    clean code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import { obtenerProductos } from "/js/main.js";
-import { existeCarro } from "/js/carro.js";
+import { mostrarProductos, obtenerProductos } from "./main.js";
+import { existeCarro } from "../js/carro.js";
 
 let ruta = "js/data_productos.json";
 
