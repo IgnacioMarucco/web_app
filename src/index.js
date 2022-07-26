b8b25cef033883178013b04bb35df6bc5ea16b94
ignacio <ignaciomarucco@gmail.com>
2022-07-26 16:29:43 -0300

    modularizar codigo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { obtenerProductos } from "./App.js";
 import { existeCarro } from "./components/carro.js";
 import { mostrarUsuario } from "./components/login.js";
 
-let ruta = "js/data_productos.json";
+let ruta = "src/data/data_productos.json";
 
 document.addEventListener('DOMContentLoaded', () => {
   obtenerProductos(ruta);
--- src/productos.js
+++ src/productos.js
@@ -2,7 +2,7 @@ import { obtenerProductos } from "../src/App.js";
 import { existeCarro } from "../src/components/carro.js";
 import { mostrarUsuario } from "../src/components/login.js";
 
-let ruta = "../js/data_productos.json";
+let ruta = "../src/data/data_productos.json";
 
 document.addEventListener('DOMContentLoaded', () => {
   obtenerProductos(ruta);
